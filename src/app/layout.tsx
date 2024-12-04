49e782db7614a9162fd2973805fe86f99c8a2d41
Dotado <oguntolagifted@gmail.com>
2024-12-04 10:01:22 +0100

    Removed google provider from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 // import { Metadata } from "next"
 import "styles/globals.css"
 import "../i18n"
-import { GoogleOAuthProvider } from "@react-oauth/google"
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
 
@@ -13,11 +12,9 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
-      <GoogleOAuthProvider clientId="44428971545-rh81erp247fo3p3dali2c052p0psu77l.apps.googleusercontent.com">
-        <body className="">
-          <main className="relative">{props.children}</main>
-        </body>
-      </GoogleOAuthProvider>
+      <body className="">
+        <main className="relative">{props.children}</main>
+      </body>
     </html>
   )
 }
